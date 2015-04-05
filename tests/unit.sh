3bc639be3ec7fdc3ef82c40f8fa2128e2b1c3cd0
Seth House <seth@eseth.com>
2015-04-05 11:08:08 -0600

    Added unit test for _format_json()

--- tests/unit.sh
+++ tests/unit.sh
@@ -15,4 +15,25 @@ _main() {
     exit $ret
 }
 
+test_format_json() {
+    # Test output without filtering through jq.
+
+    local output expected_out
+
+    $SCRIPT -j _format_json foo=Foo bar=123 baz=true qux=Qux=Qux quux='Multi-line
+string' | {
+        read -r output
+
+        expected_out='{"baz": true, "quux": "Multi-line\nstring", "foo": "Foo", "qux": "Qux=Qux", "bar": 123}'
+
+        if [ "$expected_out" = "$output" ] ; then
+            return 0
+        else
+            printf 'Expected output does not match output: `%s` != `%s`\n' \
+                "$expected_out" "$output"
+            return 1
+        fi
+    }
+}
+
 _main "$@"
