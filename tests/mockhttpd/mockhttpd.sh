2e94400633888fde4aa63dcefd11ba50822c9325
Seth House <seth@eseth.com>
2015-08-29 00:19:02 -0600

    Add function to mimic GitHub Link pagination header responses

--- tests/mockhttpd/mockhttpd.sh
+++ tests/mockhttpd/mockhttpd.sh
@@ -5,6 +5,7 @@
 #   socat TCP-L:8011,crlf,reuseaddr,fork EXEC:./mockhttpd.sh
 
 BASEDIR="$(dirname $0)"
+OK_MPORT="${OK_MPORT:=8011}"
 
 response() {
     # Format an HTTP response
@@ -65,14 +66,59 @@ find_response() {
     fi
 }
 
+pagination() {
+    # Mimic GitHub's next/prev Link header
+    #
+    local path="${1:?Path is required.}"
+    #   The HTTP request path.
+
+    awk -v "path=${path}" -v "port=${OK_MPORT}" '
+    function genlink(num, rel) {
+        return "<http://localhost:" port "/test_pagination?page=" num \
+            ">; rel=\"" rel "\""
+    }
+
+    function genlinks(curnum,   first, last, links) {
+        first = 1; last = 4; links = ""
+
+        links = genlink(first, "first")
+        links = links ", " genlink(last, "last")
+
+        if (curnum != first) links = links ", " genlink(curnum - 1, "prev")
+        if (curnum != last) links = links ", " genlink(curnum + 1, "next")
+
+        return links
+    }
+
+    BEGIN {
+        page_idx = match(path, /\?page=[0-9]+/)
+
+        if (page_idx == 0) {
+            page_num = 1
+        } else {
+            page_num = substr(path, page_idx + length("?page="))
+        }
+
+        links = genlinks(page_num)
+
+        printf("HTTP/1.1 200 OK\n")
+        printf("Link: %s\n", links)
+        printf("\nCurrent page: %s\n", page_num)
+    }
+    '
+}
+
 main() {
     # stdin is the request; stdout is the response.
 
     local method path proto
     read -r method path proto
 
+    printf 'Processing %s request for %s\n' "$method" "$path" 1>&2
+
     case $path in
         /test_error) response 500 'Server-side error';;
+        /test_pagination*) pagination "$path";;
         *) find_response "$method" "$path";;
     esac
 }
